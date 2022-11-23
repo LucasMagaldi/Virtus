3f9c7f35cac75fc18d0731c9dc7957f7daa11199
LucasMagaldi <lucas.magaldi@hotmail.com>
2022-11-22 21:20:40 -0300

    folders structure

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@ import { Router, Express, Request, Response } from "express";
 
 function routes(app: Express) {
     app.get("/test", (req: Request, res: Response) => {
-        res.send("Oi")
+        return res.status(201).json({response:"Success"});
     })
 }
 
